90b630ea24d0d1e28200c66dd8844cb813784756
Blake C Martin <blakeguitar@users.noreply.github.com>
2018-06-28 22:41:18 -0500

    ssSetting ...Add file to repository README.MD ...Update readme with listing of files added to repository

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # vb.net
 VB.NET Dynamic Link Libraries
+
+ssEXIF
+Obtains EXIF attributes for images
+
+ssFile
+Peforms various file functions
+
+ssFolder
+Performs various folder functions
+
+ssSetting
+Allows settings to be obtained and set
+
---
+++ ssSetting.vb
@@ -0,0 +1,59 @@
+﻿Public Class ssSetting
+    Public Sub Value(ByRef parmSetting As Object, ByVal parmValue As Object)
+
+        'Boolean
+        If TypeOf parmSetting Is Boolean Then
+            If TypeOf parmValue Is Boolean Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'Number
+        If TypeOf parmSetting Is Integer Then
+            If TypeOf parmValue Is Integer Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'String
+        If TypeOf parmSetting Is String Then
+            If TypeOf parmValue Is String Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'parmSetting = parmValue
+        My.Settings.Save()
+
+    End Sub
+
+    Public Sub SetValue(ByRef parmSetting As Object, ByVal parmValue As Object)
+
+        'Boolean
+        If TypeOf parmSetting Is Boolean Then
+            If TypeOf parmValue Is Boolean Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'Number
+        If TypeOf parmSetting Is Integer Then
+            If TypeOf parmValue Is Integer Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'String
+        If TypeOf parmSetting Is String Then
+            If TypeOf parmValue Is String Then
+                parmSetting = parmValue
+            End If
+        End If
+
+        'parmSetting = parmValue
+        My.Settings.Save()
+
+    End Sub
+
+
+End Class
